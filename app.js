eb84e51705a0b2b2093f327ca0a71d24013ac686
chetan chopda <chetan.ggd@gmail.com>
2016-10-08 14:56:50 +0530

    Add multiple browser tabs for single client

--- app.js
+++ app.js
@@ -3,31 +3,38 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server)
 
-var users = [];
-var connections = [];
+var users = {};
 
+var port = process.env.PORT || 2000;
 
 app.get('/',function(request, resolve){
 	resolve.sendFile(__dirname + '/index.html');
 })
 
-//app.use('/client',express.static(__dirname + '/client'));
+console.log("listening at "+port)
+server.listen(port);
 
-
-server.listen(2000);
-
-io.sockets.on('connection', function(socket){
-	connections.push(socket.handshake.address);	
-	console.log("socket connected"+connections.length);
-	console.log(connections)
+//socket new client connection 
+io.sockets.on('connection', function(socket){   
+	// add key value pair for ip with no_of_browser_tabs_open
+    if(!users[socket.handshake.headers['x-forwarded-for']]) 
+		users[socket.handshake.headers['x-forwarded-for']] = 1
+	else
+		users[socket.handshake.headers['x-forwarded-for']] += 1;
+	console.log(users)
 	setInterval(function(){
-		socket.emit('users',  {date: connections});
-	},1000)
+		socket.emit('users',  {users: users});
+	},1000);
 
-	//Disconnect
+	//Disconnect	
 	socket.on('disconnect', function(){
-		connections.splice(connections.indexOf(socket),1);
-		console.log('Disconnected'+connections.length);	
+		//remove one browser_tab_count from client ip 
+		users[socket.handshake.headers['x-forwarded-for']] -=1;
+		//no tabs are exist then remove client entry
+		if(users[socket.handshake.headers['x-forwarded-for']]  == 0)
+			delete users[socket.handshake.headers['x-forwarded-for']];
+		console.log(users)
+
 	})
 	
 
